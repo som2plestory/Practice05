2270c20b0e2f3a7d7ca42adcee41cab52d969e2c
이지희 <som2plestory@naver.com>
2022-04-25 19:25:32 +0900

    2022-04-25

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,11 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
-       
-        //메소드 내용작성
-        
-    }
-
+	
+	  public static String concatString(String[] str){
+	  
+		  //메소드 내용작성
+		  String strA = str[0]+str[1]+str[2];
+		  return strA;
+	  }
 }
--- src/com/javaex/ex09/StringUtilApp.java
+++ src/com/javaex/ex09/StringUtilApp.java
@@ -5,9 +5,12 @@ public class StringUtilApp {
     public static void main(String[] args) {
 
         String[] strArray = {"SuperMan", "BatMan", "SpiderMan"};
-        String resultStr = StringUtil.concatString(strArray);
+       
+		
+		String resultStr = StringUtil.concatString(strArray);
+		  
+		System.out.println("결과 문자열:" + resultStr);
         
-        System.out.println("결과 문자열:" + resultStr);
     }
 
 }
